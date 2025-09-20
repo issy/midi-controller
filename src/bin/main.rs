6efaca012a1bf1bbdd77a5d89e87e470d93e2669
issy <issy@issy.dev>
2025-09-20 20:34:35 +0100

    Working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,15 +6,14 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use alloc::boxed::Box;
 use embassy_executor::Spawner;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex};
 use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Timer};
 use esp_hal::{clock::CpuClock};
 use esp_hal::timer::timg::TimerGroup;
 use esp_println::println;
-use esp_hal::gpio::{AnyPin, Input, InputConfig, Pin};
+use esp_hal::gpio::{Input, InputConfig};
 
 #[panic_handler]
 fn panic(panic_info: &core::panic::PanicInfo) -> ! {
@@ -28,35 +27,27 @@ extern crate alloc;
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-struct App {
-    button_ch: &'static Channel<NoopRawMutex, u8, 4>,
-}
+static CHANNEL: Channel<CriticalSectionRawMutex, u8, 16> = Channel::new();
 
-impl App {
-    fn spawn_tasks(&'static self, spawner: embassy_executor::Spawner, button1: Input<'static>, button2: Input<'static>) {
-        spawner.spawn(button_task(button1, 0, self.button_ch)).unwrap();
-        spawner.spawn(button_task(button2, 1, self.button_ch)).unwrap();
-        spawner.spawn(logger_task(self.button_ch)).unwrap();
-    }
-}
-
-#[embassy_executor::task]
-async fn button_task(mut button: Input<'static>, id: u8, ch: &'static Channel<NoopRawMutex, u8, 4>) {
+#[embassy_executor::task(pool_size = 6)]
+async fn button_task(mut button: Input<'static>, id: u8) {
     loop {
-        button.wait_for_falling_edge().await;
-        let _ = ch.send(id).await;
-        Timer::after(Duration::from_millis(50)).await;
-
         button.wait_for_rising_edge().await;
-        let _ = ch.send(id).await;
+        println!("Button pressed");
+        let _ = CHANNEL.send(id).await;
+        Timer::after(Duration::from_millis(50)).await;
+
+        button.wait_for_falling_edge().await;
+        println!("Button released");
+        let _ = CHANNEL.send(id).await;
         Timer::after(Duration::from_millis(50)).await;
     }
 }
 
 #[embassy_executor::task]
-async fn logger_task(ch: &'static Channel<NoopRawMutex, u8, 4>) {
+async fn logger_task() {
     loop {
-        let id = ch.receive().await;
+        let id = CHANNEL.receive().await;
         println!("Button {} pressed/released", id);
     }
 }
@@ -71,16 +62,15 @@ async fn main(spawner: Spawner) {
     let timer0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timer0.timer0);
 
-    let _spawner = spawner;
-
     // Initialize buttons
-    let button1: Input = Input::new(peripherals.GPIO5.degrade(), InputConfig::default());
-    let button2: Input = Input::new(peripherals.GPIO18.degrade(), InputConfig::default());
+    spawner.spawn(button_task(Input::new(peripherals.GPIO16, InputConfig::default()), 1)).unwrap();
+    spawner.spawn(button_task(Input::new(peripherals.GPIO17, InputConfig::default()), 2)).unwrap();
+    spawner.spawn(button_task(Input::new(peripherals.GPIO5, InputConfig::default()), 3)).unwrap();
+    spawner.spawn(button_task(Input::new(peripherals.GPIO18, InputConfig::default()), 4)).unwrap();
+    spawner.spawn(button_task(Input::new(peripherals.GPIO19, InputConfig::default()), 5)).unwrap();
+    spawner.spawn(button_task(Input::new(peripherals.GPIO21, InputConfig::default()), 6)).unwrap();
 
-    // Create app and spawn tasks
-    let button_ch = Channel::<NoopRawMutex, u8, 4>::new();
-    let app: App = App { button_ch: &button_ch };
-    app.spawn_tasks(spawner, button1, button2);
+    spawner.spawn(logger_task()).unwrap();
 
     loop {
         Timer::after(Duration::from_secs(1)).await;
